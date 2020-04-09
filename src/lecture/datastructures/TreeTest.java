0800cdad5d5e95321c0542fa4a540eec0c701365
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2020-04-09 12:54:00 +0200

    Tree template

--- src/lecture/datastructures/TreeTest.java
+++ src/lecture/datastructures/TreeTest.java
@@ -1,26 +1,60 @@
 package lecture.datastructures;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TreeTest {
 
-    
+    Tree<String> testTree = null;
 
-    @Test
-    void add() {
+    @BeforeEach
+    void setup(){
+        testTree = new Tree<String>();
     }
 
     @Test
-    void contains() {
+    void addAndContains() {
+        testTree.add("eins");
+        testTree.add("zwei");
+        testTree.add("drei");
+
+        Assertions.assertTrue(testTree.contains("zwei"));
+    }
+
+    @Test
+    void containsNotExistingElement(){
+        testTree.add("eins");
+        testTree.add("zwei");
+        testTree.add("drei");
+
+        Assertions.assertFalse(testTree.contains("empty"));
+    }
+
+    @Test
+    void containsOnEmptyTree(){
+        Assertions.assertFalse(testTree.contains("empty"));
     }
 
     @Test
     void find() {
+        String testString = "zwei";
+
+        testTree.add("eins");
+        testTree.add(testString);
+        testTree.add("drei");
+
+        Assertions.assertEquals(testString, testTree.find(testString));
     }
 
     @Test
     void size() {
+        testTree.add("eins");
+        testTree.add("zwei");
+        testTree.add("drei");
+
+        Assertions.assertEquals(3, testTree.size());
     }
 }
