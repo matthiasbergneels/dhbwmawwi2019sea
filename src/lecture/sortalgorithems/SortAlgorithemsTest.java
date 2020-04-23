e5f415c2bb2ba7493208cf081292a924998a56ac
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2020-04-23 11:59:45 +0200

    SortAlgorithemsTest

---
+++ src/lecture/sortalgorithems/SortAlgorithemsTest.java
@@ -0,0 +1,58 @@
+package lecture.sortalgorithems;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortAlgorithemsTest {
+
+    static int count = 1000;
+    static int randomRange = 3000000;
+    static int[] toSort = null;
+
+    @BeforeAll
+    static void setup(){
+        generateArray();
+    }
+
+    @Test
+    void bubblesort() {
+        int[] bubbleSortedArray = SortAlgorithems.bubblesort(toSort.clone());
+        assertTrue(isSorted(bubbleSortedArray));
+    }
+
+    private static boolean isSorted(int[] array){
+        for(int i = 0; i < array.length-1; i++){
+            if(array[i] > array[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private static void generateArray(){
+        toSort = new int[count];
+        for(int i = 0; i < count; i++){
+            boolean notInserted = true;
+            while(notInserted){
+                int randomNumber = (int)(Math.random() * randomRange);
+
+                boolean found = false;
+                for(int j = 0; j < i; j++){
+                    if(toSort[j] == randomNumber){
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(!found){
+                    toSort[i] = randomNumber;
+                    notInserted = false;
+                }
+
+            }
+        }
+    }
+}
